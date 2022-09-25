798b26e5bcfa4c0994fec5a2e7914864d635b410
a-chun-lit <akariakaringo.20021226@gmail.com>
2022-09-25 22:09:16 +0900

    09252200

--- js/main.js
+++ js/main.js
@@ -1,32 +1,14 @@
-$(function($){
-    $('.bg-switcher').bgSwitcher({
-        images: ['images/background1.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg','images/background5.jpg'],
-        interval: 5000,
-        loop: true
-    });
+$(function ($) {
+  $('.slider').slick({
+    arrows: true,
+    autoplay: true,
+    dots: true,
 
-    $(window).scroll(function () {
-      const windowHeight = $(window).height();
-      const scroll = $(window).scrollTop();
-  
-      $(".fadein").each(function () {
-        const targetPosition = $(this).offset().top;
-        if (scroll > targetPosition - windowHeight + 200) {
-          $(this).addClass("is-fadein");
-        }
-      });
-    });
-
-    $('.slider').slick({
-        arrows: true,
-        autoplay: true,
-        dots: true,
-
-        responsive:[{
-          breakpoint: 480,
-          settings:{
-            slidesToShow: 1,
-          }
-        }]
-    });
+    responsive: [{
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
+  });
 });
