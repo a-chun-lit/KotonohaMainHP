ed6b0de01d519f97c937e819f2a9018e4232d9b5
a-chun-lit <akariakaringo.20021226@gmail.com>
2022-09-26 14:23:50 +0900

    09261400

--- js/main.js
+++ js/main.js
@@ -11,4 +11,16 @@ $(function ($) {
       }
     }]
   });
+
+  $(window).scroll(function () {
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $(".fadein").each(function () {
+      const targetPosition = $(this).offset().top;
+      if (scroll > targetPosition - windowHeight + 200) {
+        $(this).addClass("is-fadein");
+      }
+    });
+  });
 });
